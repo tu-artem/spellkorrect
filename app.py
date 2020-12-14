7ea7c434fe1eea8e9eb587b887448353e9c38b20
tu-artem <atu@honeypot.io>
2020-12-14 15:15:30 +0100

    add cors support

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from src.corrector import Corrector, CorrectorResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 corrector = Corrector()
 
 
